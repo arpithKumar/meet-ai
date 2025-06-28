5975fa8e67d88c5a3b2aa712a543510a6c32f3ce
Arpit Dubey <arpithk3@gmail.com>
2025-06-29 01:13:36 +0530

    create meetings and update meeting

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -9,7 +9,9 @@ import React from "react";
 const MeetingsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="overflow-x-scroll">{JSON.stringify(data, null, 2)}</div>
+  );
 };
 
 export default MeetingsView;
