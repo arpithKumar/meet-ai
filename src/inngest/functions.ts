6708226250cb4d17a98b5ef47d4cf3475443760e
Arpit Dubey <arpithk3@gmail.com>
2025-07-14 16:54:39 +0530

    bug fix

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -90,8 +90,7 @@ export const meetingsProcessing = inngest.createFunction(
     });
 
     const { output } = await summarizer.run(
-      "Summarize the following transcript: " +
-        JSON.stringify(transcriptWithSpeakers)
+      `Summarize the following transcript: ${JSON.stringify(transcriptWithSpeakers)}`
     );
 
     await step.run("save-summary", async () => {
