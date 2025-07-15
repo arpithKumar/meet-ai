f3e7f01a430c979400e98ae8e3b5c9aee3b8636d
Arpit Dubey <arpithk3@gmail.com>
2025-07-15 17:48:29 +0530

    redirect / to /meetings permanently

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/meetings",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
